39b58cb2c216593a60ebdb546c542cc96ed3b76f
Alexandrbig1 <alexsmagin1@gmail.com>
2024-08-15 19:17:56 -0500

    fix: updated tours modal window styles, added box-shadow for modal window

--- src/components/UI/ToursModal/ToursModal.styled.jsx
+++ src/components/UI/ToursModal/ToursModal.styled.jsx
@@ -20,6 +20,8 @@ export const ModalDiv = styled(Modal)`
   flex-direction: column;
   align-items: center;
   border-radius: 1.2rem;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 10px 15px -3px,
+    rgba(0, 0, 0, 0.1) 0px 4px 6px -2px;
 `;
 
 export const CloseModal = styled.button`
